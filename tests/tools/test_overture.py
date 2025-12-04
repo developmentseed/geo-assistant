09941023b04eca7b6f587e9d3f04739fe1b4d660
Wei Ji <23487320+weiji14@users.noreply.github.com>
2025-12-04 12:54:51 +0000

    Make test_get_place async

--- tests/tools/test_overture.py
+++ tests/tools/test_overture.py
@@ -1,14 +1,15 @@
 from langchain_core.tools.base import ToolCall
+
 from geo_assistant.tools.overture import get_place
 
 
-def test_get_place():
+async def test_get_place():
     command = get_place.invoke(
         ToolCall(
             name="get_place",
             type="tool_call",
             id="test_id",
-            args={"place_name": "Neighboourhood Cafe Lisbon"},
+            args={"place_name": "Neighbourhood Cafe Lisbon"},
         )
     )
     assert "place" in command.update
