9b202c504e621c1e7c42a2e8f6aa364f63fefb53
Daniel Wiesmann <yellowcap@users.noreply.github.com>
2025-12-05 15:39:16 +0000

    Render naip and summarize (#20)
    * Intermediate
    
    * Fix naip geom handling
    
    * Fix imagery decoding for summary tool
    
    * Re enable xfail
    
    * Re enable xfail
    
    * Remove png references

--- tests/test_api.py
+++ tests/test_api.py
@@ -20,8 +20,7 @@ async def initialized_app():
 
 
 @pytest.mark.xfail
-async def test_hello_world(initialized_app):
-    """Hello world test for the API"""
+async def test_call_api(initialized_app):
     async with AsyncClient(
         transport=ASGITransport(app=initialized_app),
         base_url="http://test",
@@ -33,7 +32,7 @@ async def test_hello_world(initialized_app):
                 "agent_state_input": {
                     "messages": [
                         {
-                            "content": "Find the Neighbourhood Cafe in Lisbon and buffer 0.5km around it",
+                            "content": "Find The Whitney Hotel Boston and buffer 0.1km around it, then fetch the NAIP imagery for the area from 2021 and summarize the contents of the image.",
                             "type": "human",
                         },
                     ],
@@ -43,6 +42,7 @@ async def test_hello_world(initialized_app):
                 "thread_id": str(thread_id),
             },
         )
+        print(response)
 
         assert response.status_code == 200
         assert response.headers["content-type"] == "application/x-ndjson; charset=utf-8"
