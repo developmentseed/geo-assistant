f7e777a6868c2f4cf571eb2ea7f2a60354bf8fd0
Martha <martha@developmentseed.org>
2025-12-05 15:51:31 +0000

    fix linting

--- src/geo_assistant/tools/__init__.py
+++ src/geo_assistant/tools/__init__.py
@@ -6,7 +6,7 @@ from geo_assistant.tools.summarize import summarize_sat_img
 __all__ = [
     "fetch_naip_img",
     "get_place",
+    "get_places_within_buffer",
     "get_search_area",
     "summarize_sat_img",
-    "get_places_within_buffer",
 ]
