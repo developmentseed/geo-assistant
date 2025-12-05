dddac818ea66e5b588c92b0453433be245a278e2
Soumya Ranjan Mohanty <vnn.soumya2@gmail.com>
2025-12-05 20:16:45 +0530

    Add documentation page using DevSeed's mkdocs format (#19)
    * Add devseed mkdocs basic template
    * Add docs GitHub Actions CI workflow
    * Add placeholder for tools & agents
    * Add mkdocstrings to automatically create documentation from code
    * Fix RUF022 `__all__` is not sorted
    * Deploy to GitHub Pages from artifacts
    
    Also move permissions from workflow-level to job-level.
    
    ---------
    
    Co-authored-by: Wei Ji <23487320+weiji14@users.noreply.github.com>

---
+++ src/geo_assistant/tools/__init__.py
@@ -0,0 +1,6 @@
+from geo_assistant.tools.buffer import get_search_area
+from geo_assistant.tools.naip import fetch_naip_img
+from geo_assistant.tools.overture import get_place
+from geo_assistant.tools.summarize import summarize_sat_img
+
+__all__ = ["fetch_naip_img", "get_place", "get_search_area", "summarize_sat_img"]
