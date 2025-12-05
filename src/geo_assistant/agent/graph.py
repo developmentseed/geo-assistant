8f0239c1c959ce7550d37a3c78e9f7ffeef2e855
Leo Thomas <leothomas@users.noreply.github.com>
2025-12-05 08:53:04 +0000

    Tool/naip fetcher (#12)
    * Add initial NAIP fetcher
    
    * Swap to Element84's EarthSearch API for NAIP STAC search and download
    
    * clip to bounds of aoi
    
    * Swap to Element84's EarthSearch API for NAIP STAC search and download
    
    * rename bands and remove dask chunking
    
    * Add DS_Store to .gitignore
    
    * restrict date range for naip test
    
    * Adjust timerange for tests
    
    * Add xarray to pyproj
    
    * Reduce aoi size
    
    * revert test to use tmp path
    
    * Update return types for tool to ensure state gets updated
    
    * Mark naip test as xfail
    
    * Fix geom creation
    
    ---------
    
    Co-authored-by: lillythomas <lillyelizathomas@gmail.com>
    Co-authored-by: Daniel Wiesmann <yellowcap@users.noreply.github.com>

--- src/geo_assistant/agent/graph.py
+++ src/geo_assistant/agent/graph.py
@@ -4,13 +4,16 @@ from langgraph.checkpoint.memory import InMemorySaver
 from langchain.agents import create_agent
 from geo_assistant.agent.state import GeoAssistantState
 from geo_assistant.agent.llms import llm
+from geo_assistant.tools.naip import fetch_naip_img
 from geo_assistant.tools.overture import get_place
 from geo_assistant.tools.buffer import get_search_area
 
 SYSTEM_PROMPT = """
 You are a helpful assistant that can answer questions and help with tasks.
 
-You have location and division tools available to you. Only use this data if the user asks for it.
+You have access to the following tools:
+- Overture location lookup tool: use this to get geographic information about locations in the US based on the user's query
+- NAIP imagery fetch tool: use this to fetch NAIP aerial imagery for a given area of interest returned by the overture location lookup tool and date range (do your best to extract the date range from the user's query if provided, otherwise ask the user to specify a date range)
 
 The current date and time is {now}.
 """
@@ -20,7 +23,11 @@ async def create_graph():
     checkpointer = InMemorySaver()
     graph = create_agent(
         model=llm,
-        tools=[get_place, get_search_area],
+        tools=[
+            get_place,
+            get_search_area,
+            fetch_naip_img,
+        ],
         system_prompt=SYSTEM_PROMPT.format(
             now=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         ),
