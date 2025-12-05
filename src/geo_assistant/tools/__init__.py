3efe5679b87a62d270dada68db8a53b6e11b23cd
Wei Ji <23487320+weiji14@users.noreply.github.com>
2025-12-05 12:23:28 +0000

    Fix RUF022 `__all__` is not sorted

--- src/geo_assistant/tools/__init__.py
+++ src/geo_assistant/tools/__init__.py
@@ -3,4 +3,4 @@ from geo_assistant.tools.naip import fetch_naip_img
 from geo_assistant.tools.overture import get_place
 from geo_assistant.tools.summarize import summarize_sat_img
 
-__all__ = ["get_place", "fetch_naip_img", "summarize_sat_img", "get_search_area"]
+__all__ = ["fetch_naip_img", "get_place", "get_search_area", "summarize_sat_img"]
